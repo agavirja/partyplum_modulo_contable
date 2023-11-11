20d13d55d845635e9ee5b27475a63759bcc1b1c5
agavirja <32424558+agavirja@users.noreply.github.com>
2023-11-11 13:44:50 -0500

    actualziar

--- Home.py
+++ Home.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.set_page_config(layout="wide")
 
 # streamlit run D:\Dropbox\Empresa\PartyPlum\app_matina_eventos\Home.py
 # https://streamlit.io/
 # pipreqs --encoding utf-8 "D:\Dropbox\Empresa\PartyPlum\app_matina_eventos"
-#st.set_page_config(page_icon ="https://personal-data-bucket-online.s3.us-east-2.amazonaws.com/partyplum/favicon_party_plum.png")
+
+#st.set_page_config(layout="wide")
+st.set_page_config(layout="wide",page_icon ="https://personal-data-bucket-online.s3.us-east-2.amazonaws.com/partyplum/favicon_party_plum.png")
 st.image('https://personal-data-bucket-online.s3.us-east-2.amazonaws.com/partyplum_logosimbolo.png')
