63fd2b934ddd117fb5c54398d8f8ff6e70cfc852
agavirja <32424558+agavirja@users.noreply.github.com>
2023-11-10 19:18:52 -0500

    actualziar app

--- Home.py
+++ Home.py
@@ -5,3 +5,6 @@ st.set_page_config(layout="wide")
 # streamlit run D:\Dropbox\Empresa\PartyPlum\app_matina_eventos\Home.py
 # https://streamlit.io/
 # pipreqs --encoding utf-8 "D:\Dropbox\Empresa\PartyPlum\app_matina_eventos"
+
+st.image('https://personal-data-bucket-online.s3.us-east-2.amazonaws.com/partyplum_logosimbolo.png')
+
